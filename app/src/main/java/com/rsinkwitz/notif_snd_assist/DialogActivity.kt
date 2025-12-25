2b804ab7aa28ed275c592774eea0a0e812a5245f
Rainer Sinkwitz <rainer.sinkwitz@finnova.com>
2025-12-25 17:43:06 +0100

    complete

--- app/src/main/java/com/rsinkwitz/notif_snd_assist/DialogActivity.kt
+++ app/src/main/java/com/rsinkwitz/notif_snd_assist/DialogActivity.kt
@@ -3,8 +3,6 @@ package com.rsinkwitz.notif_snd_assist
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-// Datei veraltet, nach com.rsinkwitz.notif_snd_assist verschoben.
-
 class DialogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/rsinkwitz/notif_snd_assist/ToneManager.kt
+++
@@ -1,3 +0,0 @@
-package com.rsinkwitz.notif_snd_assist
-
-// Datei veraltet, nach com.rsinkwitz.notif_snd_assist verschoben.
